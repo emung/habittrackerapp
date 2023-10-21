0a3717aac8d554e28ddc57ec020843fe1b21e1d4
emung <emanuel.ungheanu@hotmail.com>
2023-10-21 20:56:49 +0200

    add routes to routes config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HabitsContainerComponent } from './habits-container/habits-container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        title: "Homepage"
+    },
+    {
+        path: 'habits/all',
+        component: HabitsContainerComponent,
+        title: "Habits container"
+    }
+];
